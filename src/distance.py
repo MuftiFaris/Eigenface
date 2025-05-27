fccf9041cce3b3da611fefdb5c75f6207998d233
Moros <muftifarisjob@gmail.com>
2025-05-27 19:44:01 +0700

    Delete src directory

--- src/distance.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def find_nearest_face(weights_train, weights_test, paths, threshold=800.0):
-    dists = np.linalg.norm(weights_train - weights_test, axis=0)
-    idx = np.argmin(dists)
-    return paths[idx], dists[idx], dists[idx] < threshold
